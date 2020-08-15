8700e9c83ffa42e5a5bb3dc05c826e411c0af264
Gabriel Barroso <gabriel.barroso.am@gmail.com>
2020-08-15 12:29:23 -0400

    Documentação e build
    Documentação e build

--- Jogo/js/plugins.js
+++ Jogo/js/plugins.js
@@ -8,6 +8,6 @@ var $plugins =
 {"name":"Community_Basic","status":true,"description":"Plugin used to set basic parameters.","parameters":{"cacheLimit":"20","screenWidth":"816","screenHeight":"624","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"auto","alwaysDash":"off"}},
 {"name":"MadeWithMv","status":false,"description":"Show a Splash Screen \"Made with MV\" and/or a Custom Splash Screen before going to main screen.","parameters":{"Show Made With MV":"true","Made with MV Image":"MadeWithMv","Show Custom Splash":"false","Custom Image":"","Fade Out Time":"120","Fade In Time":"120","Wait Time":"160"}},
 {"name":"Battleback_Fix","status":false,"description":"Fixes battleback anchoring bugs caused by using YEP_GridFreeDoodads in RPG Maker MV v1.5.","parameters":{}},
-{"name":"ARP_TitleCommandExit","status":true,"description":"v1.00 Adds an option in the title command window to close\r\ngame window.","parameters":{"Command Exit":"Sair","Show Exit":"true"}},
+{"name":"ARP_TitleCommandExit","status":false,"description":"v1.00 Adds an option in the title command window to close\r\ngame window.","parameters":{"Command Exit":"Sair","Show Exit":"true"}},
 {"name":"$MUSH_MenuOptionScreenResolution_P1","status":true,"description":"[v.1.03] Adds the screen resolution option.","parameters":{"Resolution Command Name":"Screen Resolution","Resolution Options":"[ [1088, 612] ]","Scale Graphics":"false","Reposition Sprites":"false"}}
 ];
